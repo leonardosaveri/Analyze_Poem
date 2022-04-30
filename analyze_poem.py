d2fc74afe4053cea86a7e1c3627b7e533def458c
Leonardo Saveri <75999407+leonardosaveri@users.noreply.github.com>
2022-04-30 14:16:00 +0200

    Python code

--- analyze_poem.py
+++ analyze_poem.py
@@ -1,119 +1,77 @@
 import time
+import unidecode
+import re
+
 start_time = time.time()
 
 
-def ex1(poem_filename):
-    with open(poem_filename, encoding='utf-8') as f:
-        only_one_string = []
-        s_e = []
-        tot_s_e = []
-        last_s_e = []
-        for word in f.readlines():
-            only_one_string.append(word.lower())
-        right_string = clean_list(only_one_string)
-        for index in range(len(right_string)):
-            s_e.append(sound_element(right_string[index]))
-            tot_s_e.append(len(s_e[index]))
-            last_s_e.append(s_e[index][-1])
-        return scheme(last_s_e, tot_s_e), pattern(tot_s_e), tot_s_e, last_s_e
+def clean_list(lines):
+    lines = [re.sub(r'[^A-Za-zÀÁÂÃÄÅÈÉÊËÌÍÎÏÒÓÔÕÖØÙÚÛÜÝŸàáâãäåèéêëìíîïòóôõöøùúûüýÿ]', '', file) for file in lines]
+    for i in range(len(lines)):
+        lines[i] = unidecode.unidecode(lines[i])
+    return list(map(lambda s: s.lower(), lines))
 
 
-def clean_list(only_one_string):
-    i = 0
-    only_char = ""
-    right_string = []
-    no_accent = {224: 97,
-                 225: 97,
-                 226: 97,
-                 227: 97,
-                 228: 97,
-                 229: 97,
-                 232: 101,
-                 233: 101,
-                 234: 101,
-                 235: 101,
-                 236: 105,
-                 237: 105,
-                 238: 105,
-                 239: 105,
-                 242: 111,
-                 243: 111,
-                 244: 111,
-                 245: 111,
-                 246: 111,
-                 247: 111,
-                 248: 111,
-                 249: 117,
-                 250: 117,
-                 251: 117,
-                 252: 117,
-                 253: 121,
-                 255: 121}
-    while i < len(only_one_string):
-        for char in only_one_string[i]:
-            if char.isalpha():
-                only_char += char
-            only_one_string[i] = only_char
-        right_string.append(only_one_string[i].translate(no_accent))
-        only_char = ""
-        i += 1
-    return right_string
+
+def find_se(lines):
+    se = ""
+    final_se = []
+    cnt = []
+    for i in range(len(lines)):
+        line = lines[i]
+        c = 0
+        count = 0
+        for letter in line:
+            if c + 2 > len(line):
+                se += letter
+                final_se.append(se)
+                count += 1
+                break
+            if letter not in ['a', 'e', 'i', 'o', 'u', 'j', 'y']:
+                se += letter
+            else:
+                if line[c+1] not in ['a', 'e', 'i', 'o', 'u', 'j', 'y']:
+                    se += letter
+                    se = ''
+                    count += 1
+                else:
+                    se += letter
+            c += 1
+        cnt.append(count)
+        se = ''
+    return final_se, cnt
 
 
-def sound_element(right_string):
-    vowels = ['a', 'e', 'i', 'o', 'u', 'j', 'y']
-    o = 0
-    s_e = []
-    p = ""
-    while o < len(right_string):
-        p += right_string[o]
-        if right_string[o] in vowels:
-            o += 1
-            if right_string[o] not in vowels:
-                s_e.append(p)
-                p = ""
-        else:
-            o += 1
-            if right_string[o - 1] in vowels:
-                s_e.append(p)
-                p = ""
-        if o == len(right_string) - 1:
-            s_e.append(p + right_string[o])
+def prosodic_structure(final_se):
+    l = final_se
+    structure = [None] * len(l)
+    c = 1
+    for i in range(len(structure)):
+        if None not in structure:
             break
-    return s_e
+        if isinstance(structure[i], int):
+            continue
+        for d in range(i):
+            if l[i] == l[d]:
+                structure[i] = structure[d]
+        for h in range(len(structure)):
+            if structure[h] is None:
+                structure[h] = c
+                c += 1
+                break
+    return structure
 
 
-def scheme(last_s_e, tot_s_e):
-    frequency = {}
-    i = 0
-    scheme_list = []
-    for index in range(len(last_s_e)):
-        if (last_s_e[index], tot_s_e[index]) not in frequency:
-            scheme_list.append(i)
-            frequency[last_s_e[index], tot_s_e[index]] = i
-            i += 1
-        else:
-            scheme_list.append(frequency[last_s_e[index], tot_s_e[index]])
-    return scheme_list
+def check_pattern(cnt):
+    # return [len(cnt[:i]) for i in range(3, len(cnt) // 2) if len(cnt) % i == 0 and cnt[:i] * (len(cnt) // i) == cnt]
+    for i in range(3, len(cnt) // 2):
+            if len(cnt) % i == 0 and cnt[:i] * (len(cnt) // i) == cnt:
+                return len(cnt[:i])
 
+filename = "poem.txt"
 
-def pattern(tot_s_e):
-    div = 3
-    div_list = []
-    while div < len(tot_s_e):
-        if len(tot_s_e) % div == 0:
-            div_list.append(div)
-        div += 1
-    a = 0
-    index = 0
-    while div_list[index] <= len(tot_s_e):
-        if tot_s_e[a:div_list[index]] == tot_s_e[div_list[index]:div_list[index] * 2]:
-            return div_list[index]
-        index += 1
-
-# Inseet here the filename
-filename = ""
-
-print(ex1(filename))
+lines = open(filename).read().splitlines()
+final_se, cnt = find_se(clean_list(lines))
+print(f"prosody: {prosodic_structure(cnt)}\n module: {check_pattern(cnt)}\n lengths: {cnt}\n finals: {final_se}")
 
 print("My program took", time.time() - start_time, "to run")
